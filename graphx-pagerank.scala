9298523803ce7147f07ab3a14f58318a010c9427
Miguel Blom <migueloblom@gmail.com>
2021-10-10 14:09:22 +0200

    Proof of concept on GraphX PageRank

---
+++ graphx-pagerank.scala
@@ -0,0 +1,16 @@
+import org.apache.spark.graphx.GraphLoader
+
+val graph = GraphLoader.edgeListFile(sc, "data/twitter-2010@10000.txt")
+
+val ranks = graph.pageRank(0.0001).vertices
+
+val users = sc.textFile("data/twitter-2010-ids.txt").map { line =>
+  val fields = line.split(",")
+  (fields(0).toLong, fields(1))
+}
+
+val ranksByUsername = users.join(ranks).map {
+  case (id, (username, rank)) => (username, rank)
+}
+
+println(ranksByUsername.collect().mkString("\n"))
